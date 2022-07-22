00ec4fc8681aad931d3eae8a0f8c7d216c4d5538
Adam Kiss <kiss.adam@simonyi.bme.hu>
2022-07-22 22:46:00 +0200

    Initial sw

---
+++ render.py
@@ -0,0 +1,12 @@
+#!/bin/python
+
+from os import sys
+from matplotlib import pyplot as plt
+import pandas as pd
+
+tra = pd.read_csv(sys.argv[1])
+plt.plot(tra["Frequency(Hz)"], tra["Amplitude(dBm)"])
+plt.xlabel("Frequency(Hz)")
+plt.ylabel("Amplitude(dBm)")
+
+plt.savefig(sys.argv[2])
